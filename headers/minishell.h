ed51bc3fb495efd02e2bf7969aec0277424a029c
Leo <leonardlevasseur@yahoo.com>
2023-07-04 22:53:02 +0200

    begining header

---
+++ headers/minishell.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llevasse <llevasse@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/04 22:29:40 by llevasse          #+#    #+#             */
+/*   Updated: 2023/07/04 22:50:18 by llevasse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL.H
+# define MINISHELL.H
+
+# include "../libft/libft.h"
+# include <stdio.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <signal.h>
+# include <dirent.h>
+
+#endif
