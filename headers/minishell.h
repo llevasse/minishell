b5d1d386c43d5d482ea51cd1b80e98432b407407
Leo <leonardlevasseur@yahoo.com>
2023-07-06 23:01:24 +0200

    can recognize cd

--- headers/minishell.h
+++ headers/minishell.h
@@ -6,7 +6,7 @@
 /*   By: llevasse <llevasse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/04 22:29:40 by llevasse          #+#    #+#             */
-/*   Updated: 2023/07/06 22:38:57 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/07/06 22:59:20 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ char	*ft_strsep(char **p_str, const char *delim);
 void	parse(char *input);
 t_cmd	*init_cmd(char *input);
 void	get_args(t_cmd *cmd, char *input);
-
+void	check_cmd(t_cmd *cmd);
 
 // SRCS/BUILTIN //
 void	ft_echo(void);
--- srcs/parsing/parse.c
+++ srcs/parsing/parse.c
@@ -6,7 +6,7 @@
 /*   By: llevasse <llevasse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 09:51:31 by llevasse          #+#    #+#             */
-/*   Updated: 2023/07/06 22:40:22 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/07/06 23:00:22 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,24 @@
 void	parse(char *input)
 {
 	t_cmd	*cmd;
-	int	i;
 
 	cmd = init_cmd(input);
+	check_cmd(cmd);
+	printf("\n");
+}
+
+void	check_cmd(t_cmd *cmd)
+{
+	int	i;
+	
 	if (!cmd)
 		return ;
+	if (!ft_strncmp(cmd->cmd, "cd", 2))
+		return (ft_cd());	
 	printf("%s unknown command with argument(s) ", cmd->cmd);
 	i = 0;
 	while (cmd->args && cmd->args[i])
-	{
-		printf("%s ", cmd->args[i]);
-		i++;
-	}
-	printf("\n");
+		printf("%s ", cmd->args[i++]);
 }
 
 t_cmd *init_cmd(char *input)
