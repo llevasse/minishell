d2ace8a42c512ffb360f47e3bb524564c582218f
Leo <leonardlevasseur@yahoo.com>
2023-07-07 16:05:02 +0200

    so much leaks with readline ;-;

--- srcs/parsing/env.c
+++ srcs/parsing/env.c
@@ -6,7 +6,7 @@
 /*   By: llevasse <llevasse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 11:26:58 by llevasse          #+#    #+#             */
-/*   Updated: 2023/07/07 15:42:51 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/07/07 16:02:53 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,16 @@ int	check_cmd_in_env(char *cmd)
 {
 	char	*path;
 	int	has_exec;
+	char	*p_path;
 
 	has_exec = 0;
 	path = ft_strdup(getenv("PATH"));
 	if (!path)
 		return (0);
+	p_path = path;
 	while (*path && !has_exec)
 		has_exec = check_present_in_path(cmd, ft_strsep(&path, ":"));
-	return (has_exec);
+	return ((void)p_path, has_exec);
 }
 
 /// @brief Check if cmd in a command present in path.
@@ -41,9 +43,9 @@ int	check_present_in_path(char *cmd, char *path)
 	dir_entry = readdir(current_dir);
 	while (dir_entry && strcmp(cmd, dir_entry->d_name))
 		dir_entry = readdir(current_dir);
+	if (dir_entry && !ft_strcmp(cmd, dir_entry->d_name))
+		return (false_exec(path, cmd), closedir(current_dir), 1);
 	closedir(current_dir);
-	if (dir_entry && !strcmp(cmd, dir_entry->d_name))
-		return (false_exec(path, cmd), 1);
 	return (0);
 }
 
