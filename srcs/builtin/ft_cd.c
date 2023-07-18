f79e0ca9f825df3fff7f806876259cf7cec67c22
AlphomArt <menelik.wubneh@gmail.com>
2023-07-18 11:42:25 +0200

    cd is fix with fsanitize need valgrind test

--- srcs/builtin/ft_cd.c
+++ srcs/builtin/ft_cd.c
@@ -6,36 +6,28 @@
 /*   By: llevasse <llevasse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 09:24:53 by llevasse          #+#    #+#             */
-/*   Updated: 2023/07/17 19:06:41 by mwubneh          ###   ########.fr       */
+/*   Updated: 2023/07/18 11:41:42 by mwubneh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char	*ft_clear_path(char *path)
+char	*get_new_path(char **directory, char *new_path, size_t i)
 {
-	size_t	i;
-	char	*new_path;
-	char	**directory;
-
-	i = -1;
-	directory = ft_split(path, '/');
-	free(path);
-	new_path = malloc(sizeof(char) * 1);
-	if (!new_path)
-		return (exit(-1), NULL);
-	new_path[0] = '\0';
 	while (directory[++i])
 	{
 		if (!ft_strcmp(directory[i + 1], "."))
 		{
 			new_path = ft_strjoin(ft_strjoin(new_path, "/"), directory[i]);
+			if (!directory[i + 1])
+				break ;
 			i++;
 		}
 		else if (!ft_strcmp(directory[i + 1], ".."))
 		{
-			if (directory[i + 1])
-				i += 1;
+			if (!directory[i + 1])
+				break ;
+			i += 1;
 		}
 		else
 			new_path = ft_strjoin(ft_strjoin(new_path, "/"), directory[i]);
@@ -43,18 +35,30 @@ char	*ft_clear_path(char *path)
 	return (new_path);
 }
 
+char	*ft_clear_path(char *path)
+{
+	char	*new_path;
+	char	**directory;
+
+	directory = ft_split(path, '/');
+	free(path);
+	new_path = malloc(sizeof(char) * 1);
+	if (!new_path)
+		return (exit(-1), NULL);
+	new_path[0] = '\0';
+	new_path = get_new_path(directory, new_path, -1);
+	return (new_path);
+}
+
 void	ft_cd(t_prompt *prompt)
 {
 	char	*new_path;
 
-	new_path = getenv("PWD");
 	if (!prompt->args)
 	{
 		new_path = ft_strjoin("/Users/", getenv("USER"));
 		if (chdir(new_path) == 0)
-		{
 			setenv("PWD", new_path, 1);
-		}
 		else
 			printf("echec\n");
 	}
@@ -64,9 +68,11 @@ void	ft_cd(t_prompt *prompt)
 		new_path = ft_clear_path(new_path);
 		if (chdir(new_path) == 0)
 		{
+			printf("%s\n", new_path);
 			setenv("PWD", new_path, 1);
 		}
 		else
 			ft_printf("wrong directory\n");
 	}
 }
+
