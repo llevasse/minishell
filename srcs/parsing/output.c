2444d6a4a88d0b8ed036f59a0455d8e80bf207d2
llevasse <leonardlevasseur@yahoo.com>
2023-07-19 22:01:12 +0200

    can compile but endless loop

--- srcs/parsing/output.c
+++ srcs/parsing/output.c
@@ -6,7 +6,7 @@
 /*   By: llevasse <llevasse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 14:52:05 by llevasse          #+#    #+#             */
-/*   Updated: 2023/07/19 16:06:37 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/07/19 22:00:19 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ void	set_output(char *input, t_prompt *prompt, t_garbage *garbage)
 		i++;
 	if (!input[i])
 		return ((void)printf("Parsing error around >\n"));
-	if (get_chat_pos(input + i, '>') != -1)
+	if (get_char_pos(input + i, '>') != -1)
 		return (multiple_output(input, prompt, garbage));
 	input += i;
 	while (get_char_pos(input, '$') != -1)
@@ -43,11 +43,49 @@ void	set_output(char *input, t_prompt *prompt, t_garbage *garbage)
 	}
 }
 
+char	*get_output(char *input, t_garbage *garbage)
+{
+	char	*output;
+	int		i;
+	int		j;
+	
+	i = get_char_pos(input, '>');
+	j = 0;
+	while (input[i + j] && input[i + j] == '>' && ft_isspace(input[i + j]))
+		j++;
+	while (input[i + j] && !ft_isspace(input[i + j]))
+		j++;
+	output = malloc((j + 1) * sizeof(char));
+	ft_add_garbage(&garbage, output);
+	ft_strlcpy(output, input + i, j);
+	return (output);	
+}
 
 //ex if prompt = cat text1 text2 > text3 > text4
 //will send to check_cmd cat text1 text2 > text3
 //and cat text1 text2 > text4
 void	multiple_output(char *input, t_prompt *prompt, t_garbage *garbage)
 {
+	char		*output;
+	char		*dup_input;	
+	t_prompt	*new_prompt;
 	
+	new_prompt = malloc(sizeof(struct s_prompt));
+	ft_add_garbage(&garbage, new_prompt);
+	output = get_output(input, garbage);
+	dup_input = ft_strdup(input);
+	replace_str(&dup_input, output, "", garbage);
+	printf("CC\n");
+	new_prompt->write_fd = -1;
+	new_prompt->d_quotes = prompt->d_quotes;
+	new_prompt->quotes = prompt->d_quotes;
+	new_prompt->args = NULL;
+	new_prompt->export_args = NULL;
+	new_prompt->cmd = prompt->cmd;
+	get_args(new_prompt, dup_input, garbage);
+	check_redirection(dup_input, new_prompt, garbage);
+	check_cmd(new_prompt, garbage);
+	if (new_prompt->write_fd == 1)
+		reset_stdio_fd(new_prompt);
+
 }
