f0000b7ff623443268d2d71d0303e812a4bb2c1a
AlphomArt <menelik.wubneh@gmail.com>
2023-07-25 16:20:11 +0200

    fix cd with absolute path

--- srcs/builtin/ft_cd.c
+++ srcs/builtin/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: llevasse <llevasse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 09:24:53 by llevasse          #+#    #+#             */
-/*   Updated: 2023/07/25 14:49:18 by mwubneh          ###   ########.fr       */
+/*   Updated: 2023/07/25 16:18:23 by mwubneh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	ft_cd(t_prompt *prompt)
 {
 	char	*new_path;
-	char 	cwd[PATH_MAX];
+	char	cwd[PATH_MAX];
 
 
 	new_path = getenv("PWD");
@@ -25,15 +25,21 @@ void	ft_cd(t_prompt *prompt)
 		if (chdir(new_path) == 0)
 		{
 			setenv("PWD", new_path, 1);
+			free(new_path);
 		}
 		else
-			printf("echec\n");
+			printf("Failure\n");
 	}
 	else if (prompt->args)
 	{
-		new_path = ft_strjoin(ft_strjoin(getenv("PWD"), "/"), prompt->args[0]);
+		if ((ft_strncmp(new_path, prompt->args[0], ft_strlen(new_path)) == 0))
+			new_path = ft_strjoin(ft_strjoin(getenv("PWD"), "/"), prompt->args[0]);
+		else
+			new_path = ft_strjoin("", prompt->args[0]);
+		printf("%s\n", new_path);
 		if (chdir(new_path) == 0)
 		{
+			free(new_path);
 			getcwd(cwd, PATH_MAX);
 			setenv("PWD", cwd, 1);
 		}
