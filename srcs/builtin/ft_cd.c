de4d8dab1b328a0626875f20261ddd6dfe30136b
AlphomArt <menelik.wubneh@gmail.com>
2023-07-25 16:32:24 +0200

    fix norms in cd

--- srcs/builtin/ft_cd.c
+++ srcs/builtin/ft_cd.c
@@ -6,44 +6,55 @@
 /*   By: llevasse <llevasse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 09:24:53 by llevasse          #+#    #+#             */
-/*   Updated: 2023/07/25 16:18:23 by mwubneh          ###   ########.fr       */
+/*   Updated: 2023/07/25 16:31:45 by mwubneh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/minishell.h"
+
+static void	cd_with_args(t_prompt *prompt, char *new_path, char cwd[PATH_MAX]);
+static void	cd_without_args(char *new_path);
 
 void	ft_cd(t_prompt *prompt)
 {
 	char	*new_path;
 	char	cwd[PATH_MAX];
 
-
 	new_path = getenv("PWD");
 	if (!prompt->args)
-	{
-		new_path = ft_strjoin("/Users/", getenv("USER"));
-		if (chdir(new_path) == 0)
-		{
-			setenv("PWD", new_path, 1);
-			free(new_path);
-		}
-		else
-			printf("Failure\n");
-	}
+		cd_without_args(new_path);
 	else if (prompt->args)
+		cd_with_args(prompt, new_path, cwd);
+}
+
+static void	cd_with_args(t_prompt *prompt, char *new_path, char cwd[PATH_MAX])
+{
+	if ((ft_strncmp(new_path, prompt->args[0], ft_strlen(new_path)) == 0))
+		new_path = ft_strjoin(
+				ft_strjoin(getenv("PWD"), "/"), prompt->args[0]);
+	else
+		new_path = ft_strjoin("", prompt->args[0]);
+	if (chdir(new_path) == 0)
 	{
-		if ((ft_strncmp(new_path, prompt->args[0], ft_strlen(new_path)) == 0))
-			new_path = ft_strjoin(ft_strjoin(getenv("PWD"), "/"), prompt->args[0]);
-		else
-			new_path = ft_strjoin("", prompt->args[0]);
-		printf("%s\n", new_path);
-		if (chdir(new_path) == 0)
-		{
-			free(new_path);
-			getcwd(cwd, PATH_MAX);
-			setenv("PWD", cwd, 1);
-		}
-		else
-			ft_printf("wrong directory\n");
+		free(new_path);
+		getcwd(cwd, PATH_MAX);
+		setenv("PWD", cwd, 1);
 	}
+	else
+	{
+		free(new_path);
+		ft_printf("wrong directory\n");
+	}
+}
+
+static void	cd_without_args(char *new_path)
+{
+	new_path = ft_strjoin("/Users/", getenv("USER"));
+	if (chdir(new_path) == 0)
+	{
+		setenv("PWD", new_path, 1);
+		free(new_path);
+	}
+	else
+		printf("Failure\n");
 }
