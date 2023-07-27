0250a495d5448111c3da0a7ad2755619bf1ead22
AlphomArt <menelik.wubneh@gmail.com>
2023-07-27 12:47:42 +0200

    test

--- srcs/builtin/ft_pwd.c
+++ srcs/builtin/ft_pwd.c
@@ -6,7 +6,7 @@
 /*   By: llevasse <llevasse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 09:28:07 by llevasse          #+#    #+#             */
-/*   Updated: 2023/07/17 23:10:00 by mwubneh          ###   ########.fr       */
+/*   Updated: 2023/07/27 12:45:36 by mwubneh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,9 @@ extern char	**environ;
 
 void	ft_pwd(void)
 {
+	char	path[PATH_MAX];
+
+	getcwd(path, PATH_MAX);
+	setenv("PWD", path, 1);
 	ft_printf("%s\n", getenv("PWD"));
 }
