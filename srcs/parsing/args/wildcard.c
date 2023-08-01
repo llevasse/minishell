83925d7bdd679e37fe7ddc39e6f45d76a84633ce
llevasse <leonardlevasseur@yahoo.com>
2023-08-01 17:15:15 +0200

    get files of dir

--- srcs/parsing/args/wildcard.c
+++ srcs/parsing/args/wildcard.c
@@ -6,7 +6,7 @@
 /*   By: llevasse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/31 15:41:19 by llevasse          #+#    #+#             */
-/*   Updated: 2023/07/31 17:37:07 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/08/01 17:14:17 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,42 @@ void	check_for_wildcard(t_prompt *prompt, char **arg, t_garbage *garbage)
 	printf("Pwd : %s\n", pwd);
 	(void)arg;
 	(void)prompt;
+}
+
+//TODO error handling for opendir
+
+int	get_nb_of_files(char *path)
+{
+	DIR				*current_dir;
+	struct dirent	*dir_entry;
+	int				i;
+
+	current_dir = opendir(path);
+	if (!current_dir)
+		return (0);
+	i = 0;
+	while ((dir_entry = readdir(current_dir) != NULL)
+		i++;
+	return (i);
+}
+
+char	**get_files_in_dir(char *path, t_garbage *garbage)
+{
+	DIR				*current_dir;
+	struct dirent	*dir_entry;
+	char			**files;
+	int				i;
+
+	files = malloc(sizeof(char *) * (get_nb_of_files(path) + 1));
+	ft_add_garbage(0, &garbage, files);
+	current_dir = opendir(path);
+	if (!current_dir)
+		return (0);
+	i = 0;
+	while ((dir_entry = readdir(current_dir) != NULL)
+		files[i++] = dir_entry->d_name;
+	files[i] = NULL;
+	return (files);
 }
 
 char	*get_pwd(t_garbage *garbage)
