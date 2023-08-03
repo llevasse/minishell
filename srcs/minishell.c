ddc8d8f126b2634a03cc394b220613105aed739c
llevasse <leonardlevasseur@yahoo.com>
2023-08-03 22:50:50 +0200

    fix little visual issue with ctrl+c

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: llevasse <llevasse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 09:39:09 by llevasse          #+#    #+#             */
-/*   Updated: 2023/08/03 22:47:03 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/08/03 22:49:13 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ void	handler(int sig, siginfo_t *info, void *context)
 	{
 		write(1, "^C\n", 3);
 		rl_on_new_line();
+		rl_replace_line("", 0);
 		rl_redisplay();
 	}
 	(void)info;
