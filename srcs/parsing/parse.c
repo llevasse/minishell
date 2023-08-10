3912cb652357bb4f830142178652d286497c59d7
Leo <leonardlevasseur@yahoo.com>
2023-08-10 19:56:22 +0200

    fixed bugs with cmd parsing :smile:

--- srcs/parsing/parse.c
+++ srcs/parsing/parse.c
@@ -6,7 +6,7 @@
 /*   By: llevasse <llevasse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 09:51:31 by llevasse          #+#    #+#             */
-/*   Updated: 2023/08/10 12:33:45 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/08/10 19:55:21 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,14 +68,39 @@ void	check_cmd(t_prompt *prompt, t_garbage *garbage)
 	if (!prompt->d_quotes && !prompt->quotes && \
 			check_quotes(prompt, &prompt->cmd, garbage))
 		return (check_cmd(prompt, garbage));
-	if (!prompt->quotes && check_is_env_var(&prompt->cmd, garbage))
-		return (check_cmd(prompt, garbage));
+//	if (!prompt->quotes && check_is_env_var(&prompt->cmd, garbage))
+//		return (check_cmd(prompt, garbage));
 	if (check_cmd_in_env(prompt, garbage))
 		return ;
 	else
 		false_exec(get_pwd(garbage), prompt, garbage);
 	if (errno == 127)
 		g_minishell.error_value = 127;
+}
+
+void	get_cmd(char **input, t_prompt *prompt, t_garbage *garbage)
+{
+	char	*cmd;
+
+	if ((*input)[0] == '"')
+	{
+		prompt->d_quotes = 1;
+		if (get_char_occurance(*input, '"') % 2 != 0)
+			no_end_quote(input, '"', "dquote>", garbage);
+		cmd = get_quoted_str(*input, '"', 1, garbage);
+		(*input) += 2 + get_char_pos((*input) + 1, '"');
+	}
+	else if ((*input)[0] == 39)
+	{
+		prompt->quotes = 1;
+		if (get_char_occurance(*input, 39) % 2 != 0)
+			no_end_quote(input, 39, "quote>", garbage);
+		cmd = get_quoted_str(*input, 39, 0, garbage);
+		(*input) += 2 + get_char_pos((*input) + 1, 39);
+	}
+	else
+		cmd = ft_strsep(input, " ");
+	prompt->cmd = cmd;
 }
 
 /// @brief Allocate memory and assign values to t_prompt.
@@ -98,10 +123,7 @@ t_prompt	*init_prompt(char *input, t_garbage *garbage)
 	prompt->next_cmd = NULL;
 	prompt->heredoc_fd[0] = -1;
 	len = ft_strlen(input);
-	if (input[0] == '"' || input[0] == 39)
-		prompt->cmd = get_quoted_str(input, input[0], 1, garbage);	
-	else
-		prompt->cmd = ft_strsep(&input, " ");
+	get_cmd(&input, prompt, garbage);
 	if (!*input || len == ft_strlen(prompt->cmd))
 		return (prompt);
 	get_args(prompt, input, garbage);
