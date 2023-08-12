19bcb4d553298fd381038400371fcf4a0fd768f3
AlphomArt <menelik.wubneh@gmail.com>
2023-08-12 10:54:34 +0200

    test

--- srcs/exec/exec.c
+++ srcs/exec/exec.c
@@ -17,17 +17,20 @@ extern t_minishell *g_minishell;
 
 static int ft_putstr_error(char *str, char *arg);
 static int ft_execute(char **args, int i, int tmp_fd, char **envp);
-static char	**get_exec_args(char *path, t_prompt *prompt, t_garbage *garbage);
+//static char	**get_exec_args(char *path, t_prompt *prompt, t_garbage *garbage);
 
 void	exec(char *path, t_prompt *prompt, t_garbage *garbage)
 {
 	int i;
 	int fd[2];
 	int tmp_fd;
+	(void)path;
+	(void)garbage;
 
 	i = 0;
 	tmp_fd = dup(STDIN_FILENO);
-	while (prompt->full_args[i] && prompt->full_args[i + 1]) {
+	while (prompt->full_args[i] && prompt->full_args[i + 1])
+	{
 		prompt->full_args = &prompt->full_args[i + 1];
 		i = 0;
 		while (prompt->full_args[i] && ft_strcmp(prompt->full_args[i], ";") && ft_strcmp(prompt->full_args[i], "|"))
@@ -85,35 +88,33 @@ static int ft_execute(char **args, int i, int tmp_fd, char **envp)
 	return (ft_putstr_error("error : cannot execute ", args[0]));
 }
 
-
-
-static char	**get_exec_args(char *path, t_prompt *prompt, t_garbage *garbage)
-{
-	char	**argv;
-	char	*cmd_path;
-	int		i;
-
-	if (!prompt->args)
-	{
-		prompt->args = malloc(sizeof(char *) * 2);
-		ft_add_garbage(0, &garbage, prompt->args);
-		prompt->args[0] = NULL;
-		if (!ft_strcmp(prompt->cmd, "ls"))
-			prompt->args[0] = getenv("PWD");
-		prompt->args[1] = NULL;
-	}
-	argv = malloc(sizeof(char *) * (get_tab_size(prompt->args) + 2));
-	ft_add_garbage(0, &garbage, argv);
-	cmd_path = ft_strjoin(path, "/");
-	ft_add_garbage(0, &garbage, cmd_path);
-	argv[0] = ft_strjoin(cmd_path, prompt->cmd);
-	ft_add_garbage(0, &garbage, argv[0]);
-	i = 0;
-	while (prompt->args[i])
-	{
-		argv[i + 1] = prompt->args[i];
-		i++;
-	}
-	argv[i + 1] = NULL;
-	return (argv);
-}
+//static char	**get_exec_args(char *path, t_prompt *prompt, t_garbage *garbage)
+//{
+//	char	**argv;
+//	char	*cmd_path;
+//	int		i;
+//
+//	if (!prompt->args)
+//	{
+//		prompt->args = malloc(sizeof(char *) * 2);
+//		ft_add_garbage(0, &garbage, prompt->args);
+//		prompt->args[0] = NULL;
+//		if (!ft_strcmp(prompt->cmd, "ls"))
+//			prompt->args[0] = getenv("PWD");
+//		prompt->args[1] = NULL;
+//	}
+//	argv = malloc(sizeof(char *) * (get_tab_size(prompt->args) + 2));
+//	ft_add_garbage(0, &garbage, argv);
+//	cmd_path = ft_strjoin(path, "/");
+//	ft_add_garbage(0, &garbage, cmd_path);
+//	argv[0] = ft_strjoin(cmd_path, prompt->cmd);
+//	ft_add_garbage(0, &garbage, argv[0]);
+//	i = 0;
+//	while (prompt->args[i])
+//	{
+//		argv[i + 1] = prompt->args[i];
+//		i++;
+//	}
+//	argv[i + 1] = NULL;
+//	return (argv);
+//}
