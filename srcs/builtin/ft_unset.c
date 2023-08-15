2e8c24e6d73e1d57815dc7dbad49b9da0c899971
AlphomArt <menelik.wubneh@gmail.com>
2023-08-15 11:04:34 +0200

    adding unset

--- srcs/builtin/ft_unset.c
+++ srcs/builtin/ft_unset.c
@@ -12,9 +12,37 @@
 
 #include "minishell.h"
 
+extern char **environ;
+
+static int	unset_env(char *arg, size_t size);
+
 void	ft_unset(t_prompt *prompt)
 {
+	char	path[PATH_MAX];
+
+	ft_bzero(path, PATH_MAX);
+	if (!prompt->args)
+		return ;
 	if (!ft_strcmp(prompt->args[0], "PWD"))
-		ft_printf("don't do that, it's useless and you will be block in you working directory..\n");
-	ft_printf("Cc, je suis unset");
+		setenv("OLDPWD", getcwd(path, PATH_MAX), 1);
+	if (prompt->args[0])
+		if (!unset_env(prompt->args[0], ft_strlen(prompt->args[0])))
+			return ;
+}
+
+//Setting the line with Key = ARGS to "".
+static int	unset_env(char *arg, size_t size)
+{
+	ssize_t	i;
+
+	i = -1;
+	while (environ[++i])
+	{
+		if (!ft_strncmp(environ[i], arg, size))
+		{
+			environ[i] = "";
+			return (1);
+		}
+	}
+	return (0);
 }
