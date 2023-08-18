b6971463a5791a5c8a8d0c0524103d7d1244c628
AlphomArt <menelik.wubneh@gmail.com>
2023-08-18 15:52:35 +0200

    need test for the export of pwd env after cd &pwd

--- srcs/builtin/ft_cd.c
+++ srcs/builtin/ft_cd.c
@@ -12,6 +12,8 @@
 
 #include "minishell.h"
 
+extern struct s_minishell	g_minishell;
+
 static char	*get_current_dir(char **environ);
 static int	ft_is_cd_args(char *args);
 static void	cd_with_args(t_prompt *prompt, char *new_path, char cwd[PATH_MAX]);
@@ -21,9 +23,17 @@ void	ft_cd(t_prompt *prompt)
 {
 	char	*new_path;
 	char	cwd[PATH_MAX];
+	int		i;
 
 	new_path = get_current_dir(prompt->environ);
-	ft_printf("%s\n", new_path);
+	i = -1;
+	while (g_minishell.env[++i])
+	{
+		if (!ft_strncmp(g_minishell.env[i], "PWD=", 4))
+			delete_duplicate_export("PWD");
+	}
+	insert_at_end(ft_joinf("%s%s", "PWD=", new_path),g_minishell.env,
+				  g_minishell.at_exit_garbage);
 	if (!ft_is_cd_args(prompt->full_args[1]))
 		cd_without_args(new_path);
 	else
