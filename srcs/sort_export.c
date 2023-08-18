19310ff22ec4b16dc56871d1f5dd9cdcf8cd6358
llevasse <leonardlevasseur@yahoo.com>
2023-08-18 21:48:43 +0200

    export print sorted alphabeticaly

--- srcs/sort_export.c
+++ srcs/sort_export.c
@@ -6,7 +6,7 @@
 /*   By: llevasse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 21:22:57 by llevasse          #+#    #+#             */
-/*   Updated: 2023/08/18 21:35:43 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/08/18 21:44:20 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,30 @@ int	is_tab_alpha_sorted(char **tab)
 	i = 0;
 	while (tab[i] && tab[i + 1])
 	{
-		printf("Comparing %s and %s\n", tab[i], tab[i + 1]);
 		if (ft_strncmp("_=", tab[i + 1], 2) && ft_strcmp(tab[i], tab[i + 1]) > 0)
-			return (printf("difference of %d :(\n",ft_strcmp(tab[i], tab[i + 1])));
-		printf("%s and %s are alright\n", tab[i], tab[i + 1]);
+			return (0);
 		i++;
 	}
-	return (0);
+	return (1);
 }
 
 void	sort_tab_alpha(char **tab)
 {
-	is_tab_alpha_sorted(tab);
+	int		i;
+	char	*temp;
+
+	while (!is_tab_alpha_sorted(tab))
+	{
+		i = 0;
+		while (tab[i] && tab[i + 1])
+		{
+			if (ft_strcmp(tab[i], tab[i + 1]) > 0)
+			{
+				temp = tab[i];
+				tab[i] = tab[i + 1];
+				tab[i + 1] = temp;
+			}
+			i++;
+		}
+	}
 }
