64c80957ab143ae72a0e4201497d9b9767cbe977
llevasse <leonardlevasseur@yahoo.com>
2023-08-18 10:39:52 +0200

    dont print * in export but strangely not in ft_env either :thinking:

--- srcs/builtin/ft_export.c
+++ srcs/builtin/ft_export.c
@@ -6,7 +6,7 @@
 /*   By: llevasse <llevasse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 09:27:41 by llevasse          #+#    #+#             */
-/*   Updated: 2023/08/17 22:36:28 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/08/18 10:38:26 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,8 @@ void	print_export(char **env)
 		return ;
 	while (env[i])
 	{
+		while (!ft_strncmp("_=", env[i], 2))
+			i++;
 		j = 0;
 		print = ft_split(env[i++], '=');
 		ft_add_garbage(0, &g_minishell.garbage, print);
