7378b7acfd233778b6c77d1bb96eef401401321b
mwubneh <menelik.wubneh@gmail.com>
2023-08-19 13:23:14 +0200

    PWD and OLDPWD now updated, setenv/getenv replaced

--- srcs/builtin/ft_cd.c
+++ srcs/builtin/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: llevasse <llevasse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 09:24:53 by llevasse          #+#    #+#             */
-/*   Updated: 2023/08/19 12:11:04 by mwubneh          ###   ########.fr       */
+/*   Updated: 2023/08/19 13:17:31 by mwubneh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,9 @@ extern struct s_minishell	g_minishell;
 
 
 static int	ft_is_cd_args(char *args);
-static void	cd_with_args(t_prompt *prompt, char *new_path, char cwd[PATH_MAX]);
-static void	cd_without_args(char *new_path);
+static void	cd_with_args(t_prompt *prompt, char *new_path,
+				char cwd[PATH_MAX], t_garbage *garbage);
+static void	cd_without_args(char *new_path, t_garbage *garbage);
 
 void	ft_cd(t_prompt *prompt, t_garbage *garbage)
 {
@@ -25,10 +26,14 @@ void	ft_cd(t_prompt *prompt, t_garbage *garbage)
 	char	cwd[PATH_MAX];
 
 	new_path = get_pwd(garbage);
+	sort_tab_alpha(g_minishell.env);
+	delete_duplicate_export("OLDPWD");
+	g_minishell.env = insert_alpha(ft_joinf("OLDPWD=%s", new_path),
+			g_minishell.env, g_minishell.at_exit_garbage);
 	if (!ft_is_cd_args(prompt->full_args[1]))
-		cd_without_args(new_path);
+		cd_without_args(new_path, garbage);
 	else
-		cd_with_args(prompt, new_path, cwd);
+		cd_with_args(prompt, new_path, cwd, garbage);
 }
 
 static int	ft_is_cd_args(char *args)
@@ -43,19 +48,19 @@ static int	ft_is_cd_args(char *args)
 /// @param *prompt Pointer prompt struct,
 /// @param *new_path Pointer to str,
 /// @param *garbage Pointer to garbage struct.
-static void	cd_with_args(t_prompt *prompt, char *new_path, char cwd[PATH_MAX])
+static void	cd_with_args(t_prompt *prompt, char *new_path,
+							char cwd[PATH_MAX], t_garbage *garbage)
 {
 	if (!ft_strncmp(new_path, prompt->args[0], ft_strlen(new_path)))
 		new_path = ft_strjoin(
-				ft_strjoin(getenv("PWD"), "/"), prompt->args[0]);
+				ft_strjoin(ft_getenv(g_minishell.env, "PWD", garbage),
+					"/"), prompt->args[0]);
 	else
 		new_path = ft_strjoin("", prompt->args[0]);
 	if (chdir(new_path) == 0)
 	{
 		free(new_path);
-		setenv("OLDPWD", cwd, 1);
 		getcwd(cwd, PATH_MAX);
-		setenv("PWD", cwd, 1);
 	}
 	else
 	{
@@ -64,13 +69,14 @@ static void	cd_with_args(t_prompt *prompt, char *new_path, char cwd[PATH_MAX])
 	}
 }
 
-static void	cd_without_args(char *new_path)
+static void	cd_without_args(char *new_path, t_garbage *garbage)
 {
-	new_path = ft_strjoin("/Users/", getenv("USER"));
+	new_path = ft_getenv(g_minishell.env, "HOME", garbage);
 	if (chdir(new_path) == 0)
 	{
-		setenv("PWD", new_path, 1);
-		free(new_path);
+		sort_tab_alpha(g_minishell.env);
+		delete_duplicate_export("PWD");
+		g_minishell.env = insert_alpha(ft_joinf("PWD=%s", new_path), g_minishell.env , g_minishell.at_exit_garbage);
 	}
 	else
 		ft_printf("cd Failure\n");
