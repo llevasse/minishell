fea865bacc5a91fb6fef99e3d464ecbe9d1b177f
AlphomArt <menelik.wubneh@gmail.com>
2023-08-22 22:05:50 +0200

    it's always broken

--- srcs/exec/exec_utils.c
+++ srcs/exec/exec_utils.c
@@ -6,14 +6,15 @@
 /*   By: mwubneh <mwubneh@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 23:34:30 by mwubneh           #+#    #+#             */
-/*   Updated: 2023/08/21 23:36:56 by mwubneh          ###   ########.fr       */
+/*   Updated: 2023/08/22 22:04:42 by mwubneh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ft_wait(t_prompt *prompt, int value)
+void	wait_exec(t_prompt *prompt, int value)
 {
+	close(prompt->tmp_fd);
 	waitpid(prompt->exec_pid, &value, WUNTRACED);
 	if (WIFEXITED(value))
 		errno = WEXITSTATUS(value);
@@ -26,5 +27,4 @@ void	swap_fd(t_prompt *prompt)
 	prompt->next_cmd->old_stdin = prompt->old_stdin;
 	prompt->next_cmd->exec_fd[0] = prompt->exec_fd[0];
 	prompt->next_cmd->exec_fd[1] = prompt->exec_fd[1];
-	prompt = prompt->next_cmd;
 }
