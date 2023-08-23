f7918f93d15744a385aa46521cd730c9581966f0
AlphomArt <menelik.wubneh@gmail.com>
2023-08-23 10:24:38 +0200

    exec norm fix/ output seems broken nothing beeing write

--- srcs/exec/exec_utils.c
+++ srcs/exec/exec_utils.c
@@ -6,11 +6,46 @@
 /*   By: mwubneh <mwubneh@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 23:34:30 by mwubneh           #+#    #+#             */
-/*   Updated: 2023/08/22 22:04:42 by mwubneh          ###   ########.fr       */
+/*   Updated: 2023/08/23 10:22:03 by mwubneh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int	cmp_exec(t_prompt *prompt, int i)
+{
+	if (ft_strcmp(prompt->full_args[i]->s, ";") && \
+		ft_strcmp(prompt->full_args[i]->s, "|"))
+		return (1);
+	return (0);
+}
+
+int	exec_child(t_prompt *prompt, int i, t_garbage *garbage)
+{
+	dup2(prompt->exec_fd[1], STDOUT_FILENO);
+	close(prompt->exec_fd[1]);
+	close(prompt->exec_fd[0]);
+	if (is_builtin(prompt->full_args[0]->s))
+		exec_builtin(prompt, garbage);
+	else if (ft_execute(prompt->full_args, i, prompt->tmp_fd,
+			prompt->environ))
+		return (0);
+	return (1);
+}
+
+int	redir(t_prompt *prompt, t_garbage *garbage)
+{
+	check_redirection(prompt, garbage);
+	if (prompt->has_redir == -1)
+	{
+		close(prompt->exec_fd[1]);
+		close(prompt->tmp_fd);
+		prompt->tmp_fd = prompt->exec_fd[0];
+		return (0);
+	}
+	delete_redirection(prompt->full_args);
+	return (1);
+}
 
 void	wait_exec(t_prompt *prompt, int value)
 {
