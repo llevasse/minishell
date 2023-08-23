df0cbf550dba17e2bd74d70a48182dae1f9063f0
llevasse <leonardlevasseur@yahoo.com>
2023-08-23 10:37:07 +0200

    no more still opened fd

--- srcs/builtin/builtin.c
+++ srcs/builtin/builtin.c
@@ -6,7 +6,7 @@
 /*   By: mwubneh <mwubneh@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/19 11:55:18 by mwubneh           #+#    #+#             */
-/*   Updated: 2023/08/22 14:58:19 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/08/23 10:36:40 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,6 @@ int	is_builtin(char *cmd)
 void	exec_builtin(t_prompt *prompt, t_garbage *garbage)
 {
 	close(prompt->tmp_fd);
-//	reset_stdio_fd(prompt);
 	if (!ft_strcmp(prompt->full_args[0]->s, "cd"))
 		ft_cd(prompt, garbage);
 	else if (!ft_strcmp(prompt->full_args[0]->s, "echo"))
@@ -60,6 +59,7 @@ void	exec_builtin(t_prompt *prompt, t_garbage *garbage)
 		ft_pwd(prompt, garbage);
 	else if (!ft_strcmp(prompt->full_args[0]->s, "unset"))
 		ft_unset(prompt);
+	reset_stdio_fd(prompt);
 	close(0);
 	close(1);
 	free_garbage(garbage);
