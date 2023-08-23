57af161e77d189b4477f158e8f9452b30010be3a
llevasse <leonardlevasseur@yahoo.com>
2023-08-23 23:13:58 +0200

    fix how echo check for -n flag

--- srcs/builtin/ft_echo.c
+++ srcs/builtin/ft_echo.c
@@ -6,13 +6,14 @@
 /*   By: llevasse <llevasse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 09:26:40 by llevasse          #+#    #+#             */
-/*   Updated: 2023/08/23 22:33:52 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/08/23 23:13:12 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 static void	print_args(t_arg **args, int i);
+static int is_n_flag(char *s);
 
 /// @brief recreation of the echo cmd,
 /// print passed argument.
@@ -21,21 +22,34 @@ void	ft_echo(t_prompt *prompt)
 {
 	if (!prompt->args)
 		write(1, "\n", 1);
-	else if (prompt->args[0] && !ft_strcmp(prompt->args[0]->s, "-n"))
+	else if (prompt->args[0] && is_n_flag(prompt->args[0]->s))
 	{
 		if (!prompt->args[1])
 			write(1, "", 0);
 		else
-		{
 			print_args(&prompt->args[1], -1);
-			write(1, "%\n", 2);
-		}
 	}
 	else
 	{
 		print_args(prompt->args, -1);
 		write(1, "\n", 2);
 	}
+}
+
+/// @brief Check if *s is valid -n echo flag.
+static int is_n_flag(char *s)
+{
+	int	i;
+
+	i = 1;
+	if (s[0] == '-')
+	{
+		while (s[i] && s[i] == 'n')
+			i++;
+		if (!s[i])
+			return (1);
+	}
+	return (0);
 }
 
 /// @brief Print arguments from a certain index.
