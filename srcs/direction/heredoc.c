e2792a1c72aa9d42d43064ed8c29fe81c6dc43e6
Leo <leonardlevasseur@yahoo.com>
2023-08-26 22:32:13 +0200

    fix heredoc

--- srcs/direction/heredoc.c
+++ srcs/direction/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: llevasse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/22 14:38:55 by llevasse          #+#    #+#             */
-/*   Updated: 2023/08/26 22:24:02 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/08/26 22:31:42 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,4 +80,5 @@ void	write_heredoc(t_prompt *p, char *heredoc_name, int use_env_var)
 		ft_putendl_fd(text, p->exec_fd[1]);
 	}
 	dup2(p->exec_fd[0], p->tmp_fd);
+	close(p->exec_fd[1]);
 }
