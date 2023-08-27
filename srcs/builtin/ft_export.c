33f8e276222a9d81e56ee48d1676b4e9bbc45773
llevasse <leonardlevasseur@yahoo.com>
2023-08-27 18:54:00 +0200

    idk

--- srcs/builtin/ft_export.c
+++ srcs/builtin/ft_export.c
@@ -6,7 +6,7 @@
 /*   By: llevasse <llevasse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/06 09:27:41 by llevasse          #+#    #+#             */
-/*   Updated: 2023/08/27 18:07:30 by mwubneh          ###   ########.fr       */
+/*   Updated: 2023/08/27 18:46:06 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,32 +38,27 @@ void	export_listing(char **env, int i, t_minishell *shell)
 {
 	int		j;
 	char	**print;
+	int		empty;
 
 	j = 0;
+	empty = !!(get_char_pos(env[i], '='));
 	print = ft_split(env[i++], '=');
 	ft_add_garbage(0, &shell->garbage, print, shell);
-	if (!print[j + 1])
-		printf("declare -x %s\n", print[j]);
-	else if (print[j + 1] && !ft_strcmp(print[j + 1], "\"\""))
-	{
-		printf("declare -x %s=\"\"\n", print[j]);
-		print[j + 1] = "";
-	}
+	if (!empty)
+		printf("declare -x %s", print[j]);
 	else
-	{
 		printf("declare -x %s=\"", print[j]);
+	ft_add_garbage(0, &shell->garbage, print[j++], shell);
+	while (print[j])
+	{
+		printf("%s", print[j]);
 		ft_add_garbage(0, &shell->garbage, print[j++], shell);
-		while (print[j])
-		{
-			printf("%s", print[j]);
-			ft_add_garbage(0, &shell->garbage, print[j++], shell);
-			if (print[j])
-				printf("=");
-		}
-		if (j > 1)
-			printf("\"");
-		printf("\n");
+		if (print[j])
+			printf("=");
 	}
+	if (empty)
+		printf("\"");
+	printf("\n");
 }
 
 /// @brief Delete first instance found of *key in shell->env.
@@ -80,23 +75,6 @@ void	delete_duplicate_export(char *key, t_minishell *shell)
 		delete_element_at_index(shell->env, i);
 }
 
-void	export_empty(t_prompt *p, char *exports, t_export *exp)
-{
-	while (p->export_args && p->export_args->key && !p->export_args->content)
-	{
-		if (!ft_strncmp(exp->key, "_=", 2))
-			return ;
-		exports = ft_joinf("%s", exp->key);
-		if (!exports)
-			return ;
-		ft_add_garbage(1, &p->shell->at_exit_garbage, exports, p->shell);
-		delete_duplicate_export(exp->key, p->shell);
-		p->shell->env = insert_at_end(exports,
-				p->shell->env, p->shell);
-		p->export_args = p->export_args->next;
-	}
-}
-
 /// @brief Reproduce export builtin behavior.
 /// @param *prompt Pointer to prompt struct.
 void	ft_export(t_prompt *p)
@@ -109,24 +87,22 @@ void	ft_export(t_prompt *p)
 	sort_tab_alpha(temp);
 	if (!p->export_args)
 		return (print_export(temp, p->shell));
-	while (p->export_args && p->export_args->key)
+	while (p->export_args)
 	{
 		exp = p->export_args;
 		if (!ft_strncmp(exp->key, "_=", 2))
 			return ;
-		if (!p->export_args->content)
-			export_empty(p, exports, exp);
 		else
 		{
 			exports = ft_joinf("%s=%s", exp->key, exp->content);
-			printf("%s\n", exports);
 			if (!exports)
 				return ;
+			printf("|%s|\n", exports);
 			ft_add_garbage(1, &p->shell->at_exit_garbage, exports, p->shell);
 			delete_duplicate_export(exp->key, p->shell);
 			p->shell->env = insert_at_end(exports,
 					p->shell->env, p->shell);
-			p->export_args = p->export_args->next;
 		}
+		p->export_args = p->export_args->next;
 	}
 }
