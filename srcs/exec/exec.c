935a9169a49e260ac1fe39721ccacaf89ab980a8
Leo <leonardlevasseur@yahoo.com>
2023-08-28 14:19:18 +0200

    au moins c;est pas bloque

--- srcs/exec/exec.c
+++ srcs/exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: mwubneh <mwubneh@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/17 13:38:23 by mwubneh           #+#    #+#             */
-/*   Updated: 2023/08/27 23:29:23 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/08/28 14:16:57 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,8 @@ void	exec(t_prompt *prompt)
 			prompt = prompt->next_cmd;
 			i = 0;
 		}
+		else
+			break ;
 	}
 	pls_wait(temp);
 }
