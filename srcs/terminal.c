42b8b2531dee43b44a6f93dce7cadc6c26543a28
llevasse <leonardlevasseur@yahoo.com>
2023-09-02 14:02:59 +0200

    fix print error with ctrl+c

--- srcs/terminal.c
+++ srcs/terminal.c
@@ -6,7 +6,7 @@
 /*   By: mwubneh <mwubneh@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 23:04:22 by mwubneh           #+#    #+#             */
-/*   Updated: 2023/09/02 13:44:02 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/09/02 14:02:12 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,8 +49,10 @@ void	handler(int sig, siginfo_t *info, void *context)
 		if (info->si_pid != 0)
 		{
 			write(1, CTRL_C, 3);
+			rl_replace_line("", 0);
+			rl_redisplay();
 			write(1, "\33[2K\r(130)minishell >>", 22);
-			rl_replace_line("", 1);
+//			rl_redisplay();
 		}
 		g_sig = SIGINT;
 	}
